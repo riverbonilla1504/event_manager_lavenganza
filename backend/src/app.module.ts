cb562977c9cb1dd0bcad422ec44aaf22be627521
river PC <river.bonilla@campusucc.edu.co>
2024-10-24 03:05:39 -0500

    delete redundant files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
@@ -29,6 +28,6 @@ import { MailModule } from './mail/mail.module';
     MailModule
   ],
   controllers: [AppController, UsersController ],
-  providers: [AppService, UsersModule],
+  providers: [UsersModule],
 })
 export class AppModule {}
